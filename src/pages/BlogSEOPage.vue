c8bd02a1105f10d7870db590b50643bd12133059
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-06-28 22:10:52 +0900

    add seo page

---
+++ src/pages/BlogSEOPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <MainLayout>
+    <div class="container" style="padding: 20px;">
+      <a-row gutter="16" type="flex" align="middle" style="width: 100%;">
+        <a-card style="width: 100%;">
+          <a-row align="middle" type="flex" style="margin-bottom: 16px;">
+            <a-col span="6">
+              블로그 ID
+            </a-col>
+            <a-col span="18">
+              <a-input
+                style="width: 100%;"
+                placeholder="ex) dotoree0103"
+              ></a-input>
+            </a-col>
+          </a-row>
+          <a-row align="middle" type="flex" justify="end">
+            <a-space>
+              <a-button>키워드 자동 생성</a-button>
+              <a-button>View 순위 확인</a-button>
+              <a-button type="primary">검색하기</a-button>
+            </a-space>
+          </a-row>
+        </a-card>
+      </a-row>
+      <!-- Search Results -->
+      <a-card title="검색 결과">
+        <a-table></a-table>
+      </a-card>
+    </div>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from "../layouts/MainLayout.vue";
+
+export default {
+  name: "BlogSEOPage",
+  components: {
+    MainLayout,
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    onChartTypeChange(event) {
+      this.chartType = event.target.value;
+    },
+    onChartPeriodChange(event) {
+      this.chartPeriod = event.target.value;
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+    fillData() {
+      this.chartData = {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            label: "Data One",
+            fill: false,
+            tension: 0.1,
+            data: [
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+            ],
+          },
+        ],
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.container > * {
+  margin-bottom: 20px !important;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,13 @@ Vue.use(VueRouter);
 import CategoryPage from "../pages/CategoryPage.vue";
 import TrackingPage from "../pages/TrackingPage.vue";
 import BigDataSummaryPage from "../pages/BigDataSummaryPage.vue";
+import BlogSEOPage from "../pages/BlogSEOPage.vue";
 
 export const router = new VueRouter({
   routes: [
     { path: "/features/category", component: CategoryPage },
     { path: "/features/tracking", component: TrackingPage },
     { path: "/features/summary", component: BigDataSummaryPage },
-    { path: "/features/blog", component: CategoryPage },
+    { path: "/features/blog", component: BlogSEOPage },
   ],
 });
