b80870633dc2a61bb9dd989cc01fb67ab9c68839
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-06-28 21:46:46 +0900

    add tracking page

---
+++ src/pages/TrackingPage.vue
@@ -0,0 +1,151 @@
+<template>
+  <MainLayout>
+    <div class="container" style="padding: 20px;">
+      <a-row gutter="16" type="flex" align="middle" style="width: 100%;">
+        <a-card style="width: 100%;">
+          <a-row align="middle" type="flex" style="margin-bottom: 16px;">
+            <a-col span="6">마켓 선택</a-col>
+            <a-col span="18">
+              <a-select default-value="1" style="width: 100%">
+                <a-select-option value="1">스토어팜</a-select-option>
+                <a-select-option value="2">쿠팡</a-select-option>
+              </a-select>
+            </a-col>
+          </a-row>
+          <a-row align="middle" type="flex" style="margin-bottom: 16px;">
+            <a-col span="6">
+              상품 URL
+            </a-col>
+            <a-col span="18">
+              <a-input style="width: 100%;"></a-input>
+            </a-col>
+          </a-row>
+          <a-row align="middle" type="flex" style="margin-bottom: 16px;">
+            <a-col span="6">키워드 입력</a-col>
+            <a-col span="18">
+              <a-tooltip>
+                <a-select
+                  mode="tags"
+                  style="width: 100%; height: 100%;"
+                ></a-select>
+                <template slot="title">
+                  1회 최대 10개까지 입력 가능합니다
+                </template>
+              </a-tooltip>
+            </a-col>
+          </a-row>
+          <a-row align="middle" type="flex" justify="end">
+            <a-space>
+              <a-tooltip>
+                <a-button>
+                  키워드 자동생성
+                </a-button>
+                <template slot="title">
+                  태그를 자동으로 입력합니다
+                </template>
+              </a-tooltip>
+              <a-button type="primary">검색하기</a-button>
+            </a-space>
+          </a-row>
+        </a-card>
+      </a-row>
+      <!-- Search Results -->
+      <a-card title="검색 결과">
+        <a-table></a-table>
+      </a-card>
+      <a-card>
+        <a-row style="margin-bottom: 16px;">
+          <a-input-group compact>
+            <a-input style="width: 20%" placeholder="스토어명"></a-input>
+            <a-input-search
+              style="width: 30%"
+              placeholder="키워드"
+              enter-button="검색하기"
+            >
+            </a-input-search>
+          </a-input-group>
+        </a-row>
+        <a-row>
+          <a-table :columns="columns" :data-source="dataSource"> </a-table>
+        </a-row>
+      </a-card>
+      <a-card title="키워드별 예상 판매 (NPay 기준 | 최근 7일간)">
+        <a-row style="margin-bottom: 16px;">
+          <a-input-group compact>
+            <a-input-search
+              style="width: 30%"
+              placeholder="키워드 (ex) 탈모샴푸"
+              enter-button="검색하기"
+            >
+            </a-input-search>
+          </a-input-group>
+        </a-row>
+        <a-row>
+          <a-table :columns="columns" :data-source="dataSource"> </a-table>
+        </a-row>
+      </a-card>
+    </div>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from "../layouts/MainLayout.vue";
+
+// const columns = [{ title: "Name", dataIndex: "name" }];
+// const dataSource = [{ key: 1, name: "Sebastian Jeong" }];
+
+export default {
+  name: "TrackingPage",
+  components: {
+    MainLayout,
+  },
+  data() {
+    return {
+      current: ["1"],
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    onChartTypeChange(event) {
+      this.chartType = event.target.value;
+    },
+    onChartPeriodChange(event) {
+      this.chartPeriod = event.target.value;
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+    fillData() {
+      this.chartData = {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            label: "Data One",
+            fill: false,
+            tension: 0.1,
+            data: [
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+            ],
+          },
+        ],
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.container > * {
+  margin-bottom: 20px !important;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import CategoryPage from "../pages/CategoryPage.vue";
+import TrackingPage from "../pages/TrackingPage.vue";
 
 export const router = new VueRouter({
   routes: [
     { path: "/features/category", component: CategoryPage },
-    { path: "/features/tracking", component: CategoryPage },
+    { path: "/features/tracking", component: TrackingPage },
     { path: "/features/summary", component: CategoryPage },
     { path: "/features/blog", component: CategoryPage },
   ],
